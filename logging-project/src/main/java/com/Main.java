00f0777b56392685e29be1d1130ee21547261ff2
Saurabhjava <45967591+Saurabhjava@users.noreply.github.com>
2026-02-19 15:51:55 +0530

    Add files via upload

--- logging-project/src/main/java/com/Main.java
+++ logging-project/src/main/java/com/Main.java
@@ -1,9 +1,17 @@
 package com;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
-	
+	static Logger log=LogManager.getLogger(Main.class);
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		log.info("This is INFO Log");
+		log.error("This is Error Log");
+		log.warn("This is Warning Log");
+		log.fatal("This is FATAL Log");
+		log.debug("This is DEBUG Log");
 		
 	}
 
